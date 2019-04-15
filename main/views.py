d8566126a8e324e89d4b540f34198c821c36f9a7
MCristii <cristii.mocanu@gmail.com>
2019-04-15 20:14:43 +0300

    add template to django

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request, "main/home.html", {'message': 'Hellooooo World!'})
 
 # Create your views here.
 
