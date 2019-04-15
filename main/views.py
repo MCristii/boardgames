200d51bcb1a144b952922e3fc90a21f49aa8d3fd
MCristii <cristii.mocanu@gmail.com>
2019-04-15 21:31:00 +0300

    add custom user form to django

--- main/views.py
+++ main/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 def home(request):
     return render(request, "main/home.html", {'message': 'Hellooooo World!'})
 
+def formular(request):
+    return render(request, "main/formular.html")
+
+def formular_submit(request):
+    return render(request, "main/raspunsformular.html",{'nume':request.POST['nume'],'prenume':request.POST['prenume'],'trimite':request.POST['trimite']})
 # Create your views here.
 
